a40857462f4a3ce4216ebd926f4205217c8614ce
7Bssk7 <arhip.fin3@gmail.com>
2025-09-07 18:39:01 -0700

    Third commit

--- 210-lab-3A.cpp
+++ 210-lab-3A.cpp
@@ -1,20 +1,58 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
+//Restaurant structure, inside includes name, address, rating, capacity of people and status (open or closed).
 struct Restaurant{
     string name;
     string address;
     double rating;
     int capacity;
     bool isOpen;
-    
-    
-
-
 };
+
+Restaurant Place();
+void outputR(const Restaurant &);
 
 int main(){
 
+    Restaurant myRestaurant1 = Place();
+    outputR(myRestaurant1);
+
     return 0;
+}
+
+Restaurant Place(){
+    Restaurant temp;
+    char status;
+    
+    cout << "Enter the Name of the Restaurant: ";
+    getline(cin, temp.name);
+    cout << "Enter the address of the Restaurant: ";
+    getline(cin, temp.address);
+    cout << "Enter the Rating of the Restaurant(1-5): ";
+    cin >> temp.rating;
+    while((temp.rating < 1) || (temp.rating > 5)){
+        cout << "Invalid input, Rating of the Restaurant must be in the range between 1 and 5." << endl;
+        cout << "Enter the Rating of the Restaurant(1-5): ";
+        cin >> temp.rating;
+
+    }
+    cout<< "Enter the MAximum Capacity of the Restaurant: ";
+    cin >> temp.capacity;
+    cout << "Is Restaurant is Currently Open(y - yes, it is open|n - no, it is closed): ";
+    cin >> status;
+    status = tolower(status);
+    while((status != 'n') || (status != 'y')){
+        cout << "Invalid input, please answer the question again!" << endl;
+        cout << "Enter the Rating of the Restaurant(1-5): ";
+        cin >> temp.rating;
+
+    }
+
+    return temp;
+
+    
+
 }
